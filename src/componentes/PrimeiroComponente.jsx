5c441d8bb75c08695a005c2ea6b1d2ca61cef8a7
Lorena Naya Nascimento Araujo <lorena.nascimento@seop.local>
2020-01-29 14:28:27 -0300

    primeiro componente com arrow function

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function primeiro(){
+export default () => 
+<div>
+    <h1>Primeiro Componente(Arrow)!</h1>
+</div>
+
+
+
+
+/*function primeiro(){
     return <h1>Primeiro Componente!</h1>
 }
 
 export default primeiro;
 
+*/
 //export default function (){
 //    return <h1>Primeiro Componente!</h1>
 //}
